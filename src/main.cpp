66697558622a68463a12e6b191b639c839bec702
pierre-24 <pierreb24@gmail.com>
2025-12-03 22:50:34 +0100

    sweep through T in main (temp)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,44 @@
-#include <mcpp_heisenberg/geometry.hpp>
-#include <iostream>
+#include <mcpp_heisenberg/mc.hpp>
+
+constexpr uint64_t MAX = 50000;
+constexpr uint64_t N = 10;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+  mch::init_logging();
+
+  uint64_t PROD_START = MAX / 10 * 2;
+
+  arma::mat lattice = arma::eye(3, 3);
+  lattice.at(0, 0) = 2;
+  lattice.at(1, 1) = 2;
+  lattice.at(2, 2) = 100;
+
+  arma::mat positions = arma::mat(1, 3);
+
+  auto geometry = mch::Geometry("square", lattice, {{"H", 1}}, positions).to_supercell(N, N, 1);
+  auto square_hamiltonian = mch::Hamiltonian::from_geometry(geometry, {"H"}, {{"H", "H", 2.0, 1.0}});
+
+  arma::vec temperature_range = arma::linspace(1, 3, 81);
+
+  for (uint64_t iT = 0; iT < temperature_range.n_rows; ++iT) {
+    auto runner = mch::MonteCarloRunner(square_hamiltonian);
+
+    double T = temperature_range.at(iT);
+
+    arma::mat stats(MAX - PROD_START, 2);
+
+    for (uint64_t i = 0; i < MAX; ++i) {
+      runner.sweep(T);
+
+      if (i > PROD_START) {
+        stats.row(i - PROD_START) = {runner.energy(), arma::sum(runner.spins())};
+      }
+    }
+
+    LOGI << T
+         << "," << arma::mean(stats.col(0)) / runner.N()
+         << "," << arma::mean(stats.col(1)) / runner.N();
+  }
+
+  return EXIT_SUCCESS;
 }
